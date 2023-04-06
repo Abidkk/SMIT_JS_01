2000380a7c7c79b7aba905c921396580dc61b502
Abidkk <121240127+Abidkk@users.noreply.github.com>
2023-04-07 04:55:57 +0500

    SMIT_js_01
    js_01

---
+++ smit1.js
@@ -0,0 +1,153 @@
+// //            :::Task -1    ::::
+
+let num1 = prompt("Enter first number:");
+let num2 = prompt("Enter second number:");
+
+// converting to number
+let n1 = Number(num1);
+let n2 = Number(num2);
+
+// conditions
+if (n1 > n2) {
+  console.log(n1 + " is larger.");
+} else if (n2 > n1) {
+  console.log(n2 + " is larger.");
+} else {
+  console.log(" equal");
+}
+
+
+//// -__________________________________________________________________________________________________
+
+
+//      ::::   Task 2   :::: 
+
+// let value = prompt('Enter a value positive Or Negetive?'," ");
+// if (value > 0) {
+//     alert('The sign is +')
+// }else if ( value < 0){
+//     alert('The sign is -')
+// }else{
+//     alert('Zero')
+// }
+
+////_______________________________________________________________________________________________________
+
+
+
+///           ::::      Task 3      :::
+
+// var vv1 = prompt('Enter first value');
+// var vv2 = prompt('Enter second value');
+// var vv3 = prompt('Enter third value');
+// var vv4 = prompt('Enter fourth value');
+// var vv5 = prompt('Enter fifth value');
+// ////console.log(v1,v2,v3,v4,v5)
+// let v1 = Number(vv1);
+// let v2 = Number(vv2);
+// let v3 = Number(vv3);
+// let v4 = Number(vv4);
+// let v5 = Number(vv5);
+
+// //
+// let bigNum =  v1;
+
+// if (v2 > bigNum) {
+//     bigNum = v2
+    
+// }
+
+// if (v3 > bigNum) {
+//     bigNum = v3
+// }
+    
+// if (v4 > bigNum) {
+//     bigNum = v4
+    
+// }
+    
+// if (v5 > bigNum) {
+//     bigNum = v5
+    
+// }
+
+// console.log(bigNum)
+
+
+
+////____________________________________________________________________________________________________
+
+
+
+////        :::: Task 4 ::::
+// for (let i = 0; i <= 15; i++) {
+    
+//     // console.log(i)
+
+//     if (i % 2 == 0 ) {
+//         console.log(i + " is an Even number")
+        
+//     }else {
+//         console.log(i + " is an Odd number")
+//     }
+// }
+
+
+////_______________________________________________________________________________________________
+
+////         ::: Task 5 ::::
+
+// let task1 = Number(prompt('Enter marks of task-1 out of 100'));
+// let task2 = Number(prompt('Enter marks of task-2 out of 100'));
+// let sum = task1 + task2;
+// let avg = sum / 2;
+
+// if (avg < 60) {
+//     console.log('Fail')
+    
+// } else if (avg < 70) {
+//     console.log('Grade-D')
+    
+// } else if (avg < 80){
+//     console.log('Grade-C')
+// }else if (avg < 90){
+//     console.log('Grade-B')
+// }else if (avg < 100){
+//     console.log('Grade-A')
+// }
+
+
+///_____________________________________________________________________________________________________
+
+
+////        ::::  Task 6 :::
+
+// for (let i = 1; i <= 100; i++) {
+
+//    if (i % 3 == 0 && i % 5 == 0) {
+//     console.log('FizzBuzz');
+//     continue
+// } else if (i % 3 == 0){
+//     console.log('Fizz');
+//     continue
+// } else if (i % 5 == 0  ){
+//     console.log('Buzz');
+//     continue
+// }
+// console.log(i)
+// }
+
+
+
+////_______________________________________________________________________________________________
+
+////        :::::   Task 7      :::::
+
+// for (let i = 1; i<= 5 ; i++) {
+//    let star = ' ';
+//    for (let j = 1; j <= i; j++) {
+//     star += '* ';
+//    }
+//    console.log(star)
+    
+// }
